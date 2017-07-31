6ebb1376125acb8024b0e35b81bdfe7d0b32f66f
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2017-07-31 12:07:45 +0200

    removed python tiling

--- matlabdeep/imagepad.m
+++ matlabdeep/imagepad.m
@@ -10,24 +10,24 @@ function [outshape,k,i,j] = imagepad(C,xshape,field_height,field_width,padding,s
   stridey = stride(1);
   stridex = stride(2);
 
-  tile = @repmat;
+  % np.repeat(np.arange(A), B) =>  as 0000 1111 2222 ..
+  % np.tile(np.arange(A), B))  =>  as 012 012 012
+  blockrepeat0= @(A,B) reshape(repmat((1:A)-1,B,1),1,[]);
+  interrepeat0= @(A,B) reshape(repmat((1:A)-1,1,B),1,[]);
   
   assert (mod(H + padding(1)+padding(3) - field_height,stridey) == 0,'alignment of pad and stride-size in H');
   assert (mod(W + padding(2)+padding(4) - field_width,  stridex) == 0,'alignment of pad and stride-size in W');
   out_height = (H + padding(1)+padding(3) - field_height) / stridey + 1;
   out_width = (W + padding(2)+padding(4) - field_width) / stridex+ 1;
 
-  i0 = repmat(1:field_height, field_width,1); 
-  i0 = tile(i0, C);
-  i1 = stridey * repmat(1:out_height, out_width);
-  j0 = tile(1:field_width, field_height * C);
-  j1 = stridex * tile(1:out_width, out_height);
+  i0 = blockrepeat0(field_height,field_width);
+  i0 = repmat(i0, 1, C); % was np.tile(i0,C) along X
+  i1 = stridey * blockrepeat0(out_height, out_width);
+  j0 = interrepeat0(field_width,field_height*C);
+  j1 = stridex * interrepeat0(out_width, out_height);
   i = reshape(i0,[],1) + reshape(i1,1,[]);
   j = reshape(j0,[],1) + reshape(j1,1,[]);
   
-  k = reshape(repmat(1:C, field_height * field_width),[],1);
+  k = blockrepeat0(C,field_height * field_width)';
     
-  i = i';
-  j = j';
-  k = k';
   outshape = [out_height;out_width];
