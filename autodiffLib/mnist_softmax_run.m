207ff12a79a3a039e82b9e19ff8d04e55c16512e
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2017-08-01 09:47:21 +0200

    acceleration mode

---
+++ autodiffLib/mnist_softmax_run.m
@@ -0,0 +1,4 @@
+tic;s=sim('mnist_softmax','SimulationMode','accelerator');toc; s.realtout.Data(end)-s.realtout.Data(1)
+%%
+
+tic;s=sim('mnist_softmax','SimulationMode','normal');toc; s.realtout.Data-s.realtout.Data(1)
