fdb44c8b02f5cb2654be1cc350fa18c4a55c99cc
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2017-08-01 10:00:51 +0200

    also MnistSystemInt works in code gen mode

--- autodiffLib/MnistSystemInt.m
+++ autodiffLib/MnistSystemInt.m
@@ -5,16 +5,17 @@ classdef MnistSystemInt < matlab.System  & matlab.system.mixin.Propagates
     % to define a System object with discrete state.
 
     % Public, tunable properties
-    properties
+    properties(Nontunable)
         trainset = 1;
         items = 100;
     end
+
 
     properties(DiscreteState)
 
     end
 
-    % Pre-computed constants
+    % Pre-computed constants!
     properties(Access = private)
         epoch
         iteration
@@ -32,26 +33,26 @@ classdef MnistSystemInt < matlab.System  & matlab.system.mixin.Propagates
             
         end
 
-        function  [p1,p2,p3] = isOutputFixedSizeImpl(obj)
+        function  [p1,p2,p3,out4] = isOutputFixedSizeImpl(obj)
             p1 = true;
             p2= true;
             p3 =true;
          
         end
         
-            function [p1,p2,p3] = getOutputDataTypeImpl(obj)
+            function [p1,p2,p3,out4] = getOutputDataTypeImpl(obj)
                 p1 = 'double';
                 p2 = 'double';
                 p3 = 'double';
             end
             
-            function [p1,p2,p3] = isOutputComplexImpl(obj)
+            function [p1,p2,p3,out4] = isOutputComplexImpl(obj)
                 p1 = false;
                 p2 = false;
                 p3 = false;
             end
             
-        function [sz_1,sz_2,sz_3] = getOutputSizeImpl(obj) 
+        function [sz_1,sz_2,sz_3,out4] = getOutputSizeImpl(obj) 
             
               sz_1 = [obj.items,28*28];
               sz_2 = [obj.items,1];
@@ -102,13 +103,13 @@ classdef MnistSystemInt < matlab.System  & matlab.system.mixin.Propagates
                     left = min(needed,obj.n-obj.iteration);
                 end
                 if left > 0
-                    Q = obj.indices(obj.iteration:obj.iteration+left-1);
-                    x(k1:k1+left-1,:) = obj.allimages(Q,:);
-                    labels(k1:k1+left-1,:) = obj.alllabels(Q,:);
-                    labelshot(k1:k1+left-1,:) = obj.alllabelshot(Q,:);
-                    obj.iteration = obj.iteration + left;
-                    k1 = k1 + left;
-                    needed = needed - left;
+%                     Q = obj.indices(obj.iteration:obj.iteration+left-1);
+%                     x(k1:k1+left-1,:) = obj.allimages(Q,:);
+%                     labels(k1:k1+left-1,:) = obj.alllabels(Q,:);
+%                     labelshot(k1:k1+left-1,:) = obj.alllabelshot(Q,:);
+%                     obj.iteration = obj.iteration + left;
+%                     k1 = k1 + left;
+%                     needed = needed - left;
                 end
            end
         end
