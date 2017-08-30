e24c80f5c817ed6f0b4f142b420cf5c0f6db76d8
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2017-08-30 18:59:08 +0200

    conv2d and maxpool stub blocks with the constant block

---
+++ autodiffLib/conv2d_setup.m
@@ -0,0 +1,36 @@
+classdef conv2d_setup < matlab.System
+    % untitled3 Add summary here
+    %
+    % This template includes the minimum set of functions required
+    % to define a System object with discrete state.
+
+    % Public, tunable properties
+    properties
+
+    end
+
+    properties(DiscreteState)
+
+    end
+
+    % Pre-computed constants
+    properties(Access = private)
+
+    end
+
+    methods(Access = protected)
+        function setupImpl(obj)
+            % Perform one-time calculations, such as computing constants
+        end
+
+        function [kq,sz] = stepImpl(obj,X,W)
+            % Implement algorithm. Calculate y as a function of input u and
+            % discrete states.
+            y = u;
+        end
+
+        function resetImpl(obj)
+            % Initialize / reset discrete-state properties
+        end
+    end
+end
---
+++ autodiffLib/maxpool_setup.m
@@ -0,0 +1,36 @@
+classdef maxpool_setup < matlab.System
+    % untitled4 Add summary here
+    %
+    % This template includes the minimum set of functions required
+    % to define a System object with discrete state.
+
+    % Public, tunable properties
+    properties
+
+    end
+
+    properties(DiscreteState)
+
+    end
+
+    % Pre-computed constants
+    properties(Access = private)
+
+    end
+
+    methods(Access = protected)
+        function setupImpl(obj)
+            % Perform one-time calculations, such as computing constants
+        end
+
+        function [kq,sz] = stepImpl(obj,u)
+            % Implement algorithm. Calculate y as a function of input u and
+            % discrete states.
+            y = u;
+        end
+
+        function resetImpl(obj)
+            % Initialize / reset discrete-state properties
+        end
+    end
+end
