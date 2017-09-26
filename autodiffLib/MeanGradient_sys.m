51680a57f30defc5d799d311dd960e75041b4dd3
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2017-09-26 17:58:18 +0200

    added missing syst

---
+++ autodiffLib/MeanGradient_sys.m
@@ -0,0 +1,51 @@
+classdef MeanGradient_sys < matlab.System & matlab.system.mixin.Propagates
+    % untitled2 Add summary here
+    %
+    % This template includes the minimum set of functions required
+    % to define a System object with discrete state.
+
+    % Public, tunable properties
+    properties
+
+    end
+
+    properties(DiscreteState)
+
+    end
+
+    % Pre-computed constants
+    properties(Access = private)
+
+    end
+
+    methods(Access = protected)
+        function setupImpl(obj)
+            % Perform one-time calculations, such as computing constants
+        end
+
+        function  [p1] = isOutputFixedSizeImpl(obj)
+            p1 = true;         
+        end
+        
+        function [p1] = getOutputDataTypeImpl(obj)
+            p1 = propagatedInputDataType(obj,1);
+        end
+
+        function [p1] = isOutputComplexImpl(obj)
+            p1 = false;
+        end
+            
+        function [sz_1] = getOutputSizeImpl(obj) 
+            sz_1 = propagatedInputSize(obj,1); % J 
+        end
+        
+
+        function JX = stepImpl(obj,X,JY)
+            JX = ones(size(X))*(JY/numel(X));
+        end
+
+        function resetImpl(obj)
+            % Initialize / reset discrete-state properties
+        end
+    end
+end
