ddce93c8446f14dbf4a3674e79614f013c06fa69
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2017-11-03 17:52:22 +0100

    final script for ROC

--- pythontest/stats_print.py
+++ pythontest/stats_print.py
@@ -9,10 +9,10 @@ def main():
     table = []
     for x in sys.argv[1:]:
         f = json.load(open(x,"rb"))
-        table.append([f["test"],f["implementation"],f.get("simulation_mode",""),"gpu" if f["gpu"] else "","singlecore" if f.get("single_core",False) else "",f["accuracy"],f["cm_Fscore"],f["training_time"],f["testing_time"]])
+        table.append([f["test"],f["implementation"],f.get("simulation_mode",""),"gpu" if f["gpu"] else "","singlecore" if f.get("single_core",False) else "",f["accuracy"],f["cm_Fscore"],f["training_time"],f["testing_time"],x])
     table.sort(key = operator.itemgetter(0,1,2,3,4))
 
-    print tabulate.tabulate(table, headers=["test","impl","GPU","sim_mode","single","accuracy","Fscore","training time","testing time"])
+    print tabulate.tabulate(table, headers=["test","impl","GPU","sim_mode","single","accuracy","Fscore","training time","testing time","filename"])
 
 if __name__ == '__main__':
     main()
