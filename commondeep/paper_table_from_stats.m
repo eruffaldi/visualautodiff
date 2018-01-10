679eb8822b210c7738fdf70b94ca5b00dc57279e
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2018-01-10 14:57:07 +0100

    stats

--- commondeep/paper_table_from_stats.m
+++ commondeep/paper_table_from_stats.m
@@ -25,18 +25,30 @@ og = grpstats(o,{'id','test'},{@mean,@std},'DataVars',{'training_time','testing_
 % one line for every id
 out = {};
 tests = {'softmax','cnn'};
-names = {'Tensorflow GPU','Matlab GPU','Tensorflow CPU ST', 'Tensorflow CPU MT','Matlab CPU MT','Simulink Interpreted CPU S','Simulink Codegen CPU ST','Simulink Codegen BLAS CPU ST','Simulink Codegen BLAS CPU MT'};
+lightout =0;
+if lightout
+    names = {'','','','','','','','',''};
+    annotatebold= @(x) x;
+    annotatesmall = @(x) sprintf('%3f',x);
+else
+    names = {'Tensorflow GPU','Matlab GPU','Tensorflow CPU ST', 'Tensorflow CPU MT','Matlab CPU MT','Simulink Interpreted CPU S','Simulink Codegen CPU ST','Simulink Codegen BLAS CPU ST','Simulink Codegen BLAS CPU MT'};
+    annotatebold = @(x) sprintf('\textbf{%s}',x);
+    annotatesmall = @(x) sprintf('{\scriptstyle %3f}',x);
+end
 for I=1:9
     cid = sprintf('C%d',I);
-    out{I,1} = cid;
-    out{I,2} = names{I};
+    out{I,1} = annotatebold(cid);
+    out{I,2} = annotatebold(names{I});
     for J=1:length(tests)
         oI = og(strcmp(og.id,cid) & strcmp(og.test,tests{J}),:);
-        out{I,2+J*2} = sprintf('%3.1f +- {\scriptstyle %3f}',oI.mean_training_time);
-        out{I,2+J*2+1} = sprintf('%3.1f +- {\scriptstyle %3f}',oI.mean_testing_time);
+        out{I,2+J*2} = sprintf('%3.1f +- %s',oI.mean_training_time,annotatesmall(oI.std_training_time));
+        out{I,2+J*2+1} = sprintf('%3.1f +- %s',oI.mean_testing_time,annotatesmall(oI.std_testing_time));
     end
 end
-out
+out{end} = [out{end}+  '\\ \hline'];
+
+%%
+
 
 
 
