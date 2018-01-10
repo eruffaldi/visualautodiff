139ae827f305e718bed26a08ebb33859d787a00e
Emanuele Ruffaldi <e.ruffaldi@sssup.it>
2018-01-10 15:51:43 +0100

    updated

--- pythontest/mnist_cnn.all.sh
+++ pythontest/mnist_cnn.all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python mnist_cnn.py -w --p57 
-python mnist_cnn.py -w --p57 --no-gpu
-python mnist_cnn.py -w --p57 --no-gpu --singlecore
+for i in {1..10}; do echo "--------------- C1 $i"; python mnist_cnn.py -w --p57 --id C1; done
+for i in {1..10}; do echo "--------------- C4 $i"; python mnist_cnn.py -w --p57 --no-gpu --id C4; done
+for i in {1..10}; do echo "--------------- C3 $i"; python mnist_cnn.py -w --p57 --no-gpu --singlecore --id C3; done
--- pythontest/mnist_softmax.all.sh
+++ pythontest/mnist_softmax.all.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-TIMES=10
-for i in {1..$TIMES}; do python mnist_softmax.py -w --id C1; done 
-for i in {1..$TIMES}; do python mnist_softmax.py -w --no-gpu --id C4; done  
-for i in {1..$TIMES}; do python mnist_softmax.py -w --no-gpu --singlecore --id C3; done
+for i in {1..10}; do echo "--------------- C1 $i"; python mnist_softmax.py -w --id C1; done 
+for i in {1..10}; do echo "--------------- C4 $i"; python mnist_softmax.py -w --no-gpu --id C4; done  
+for i in {1..10}; do echo "--------------- C3 $i"; python mnist_softmax.py -w --no-gpu --singlecore --id C3; done
